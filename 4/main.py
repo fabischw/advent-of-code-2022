7d9f89647101ce29785a8b733e4f0c27bee75931
fabischw <107812436+fabischw@users.noreply.github.com>
2022-12-04 09:52:14 +0100

    day4 complete

---
+++ 4/main.py
@@ -0,0 +1,67 @@
+"""
+Solution for
+Day 4: 
+"""
+
+with open("data.txt") as file:
+    data = file.readlines()
+
+for idx,lines in enumerate(data):
+    data[idx] = lines.replace("\n","")
+
+
+def check_contain_full(range1_min, range1_max, range2_min, range2_max):
+    return range1_min <= range2_min <= range2_max <= range1_max or range2_min <= range1_min <= range1_max <= range2_max
+
+def check_contain_partial(range1_min, range1_max, range2_min, range2_max):
+    return range1_min <= range2_min <= range1_max  or range2_min <= range1_min <= range2_max
+
+
+
+
+def part_one():
+    count = 0
+
+    for lines in data:
+
+        #splitting the data into 2 parts (elf1, elf2)
+
+        elf1,elf2 = lines.split(sep=",")
+        elf1_min, elf1_max = elf1.split("-")
+        elf2_min, elf2_max = elf2.split("-")
+
+        elf1_min = int(elf1_min)
+        elf1_max = int(elf1_max)
+        elf2_min = int(elf2_min)
+        elf2_max = int(elf2_max)
+
+
+        if check_contain_full(elf1_min,elf1_max,elf2_min,elf2_max):
+            count += 1
+
+
+
+    print(count)
+
+
+def part_two():
+    count = 0
+
+    for lines in data:
+
+        #splitting the data into 2 parts (elf1, elf2)
+
+        elf1,elf2 = lines.split(sep=",")
+        elf1_min, elf1_max = elf1.split("-")
+        elf2_min, elf2_max = elf2.split("-")
+
+        elf1_min = int(elf1_min)
+        elf1_max = int(elf1_max)
+        elf2_min = int(elf2_min)
+        elf2_max = int(elf2_max)
+
+
+        if check_contain_partial(elf1_min,elf1_max,elf2_min,elf2_max):
+            count += 1
+
+    print(count)
