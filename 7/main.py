9e4d4dc142599140ab82ddc7bfd74b71b9771c7d
fabischw <107812436+fabischw@users.noreply.github.com>
2022-12-13 11:10:40 +0000

    saving work done on day7 solution

--- 7/main.py
+++ 7/main.py
@@ -16,24 +16,39 @@ def part_one():
     create a dictionairy with sub-dictionairies
     """
 
-    file_structure = {}
-    current_dirctory = ["/"]#saving the path to the current directory
+    file_structure = {
+        "/": None
+    }
+    cwd = ["/"]#saving the path to the current directory
 
     mode = "output"# variable to switch between output and user commands
 
     for instructions in data:
-        if instructions.startswith("$"):
-            cmd = instructions[2:]
-            print(cmd)
+        if instructions.startswith("$"):#checking if the line is a command
+            cmd = instructions[2:]#current command
             if cmd == "ls":
-                mode = "output"
+                mode = "output"#changing the mode to output -> setting up 'listener' 
             elif cmd.startswith("cd"):
                 # logic to switch to folder
                 dest = cmd[3:]
-                print(dest)
+                #getting to the previous subfolder
+                if dest == ".."and len(cwd) > 1:
+                    cwd.pop()
+                mode = "cmd"
 
-        #elif mode == "output":
 
+        elif mode == "output":
+            if instructions.startswith("dir"):
+                dirname = instructions[4:]#getting the name for the new directory
+
+
+                #getting to the current working directory
+                current = file_structure.get("/")# entry point for the loop below
+                for elements in cwd:
+                    current = current.get(str(elements))
+                
+                
+                current[dirname] = {}#setting the new directory as a key in the dict
 
 
 #def part_two():
